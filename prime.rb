2fc0ea6d1980c4c121048e1752231840249c5ae6
Sam D <samidiaz3@gmail.com>
2021-02-04 22:57:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,5 +2,5 @@
 def prime? (integer)
   #boolean return if # is a prime number
   require 'prime'
-  Prime.prime?(number) # is a prime number, true. else false. }
+  Prime.prime?(integer) # is a prime number, true. else false. }
 end
