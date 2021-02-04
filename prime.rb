8fa66a1fb554f6948e08c5f30de6761caec6ff1f
Sam D <samidiaz3@gmail.com>
2021-02-04 23:15:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,22 @@
-# Add  code here!
-def prime? (integer)
-  #boolean return if # is a prime number
-  require 'prime'
-  Prime.prime?(integer) # is a prime number, true. else false. }
-end
+# Easy Solution:
+#   def prime? (integer)
+#     require 'prime'
+#     Prime.prime?(integer)
+#   end
+
+# def prime? (integer)
+#   require 'prime'
+#   Prime.prime?(integer)
+# end
+
+#My thought:
+# if divisibale by >3 numbers but not <1, .... if divs by 1 and itself but not 2 or 3?
+# Prime numbers:
+  # - are divisibale by 0
+  # - divisibale by 1 and itself only
+  # - NOT negative
+  # - NOT div by 2 (rules out even numbers) or 3
+  # - ... how to rule out negative numbers ... 3,5,7,9...
+  def prime? (integer)
+
+  end
