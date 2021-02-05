38add7f81a406746f2248ac3af78314927abe322
Sam D <samidiaz3@gmail.com>
2021-02-05 00:17:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,19 +4,22 @@
 #     Prime.prime?(integer)
 #   end
 
-# def prime? (integer)
-#   require 'prime'
-#   Prime.prime?(integer)
-# end
 
-#My thought:
-# if divisibale by >3 numbers but not <1, .... if divs by 1 and itself but not 2 or 3?
-# Prime numbers:
-  # - are divisibale by 0
-  # - divisibale by 1 and itself only
-  # - NOT negative
-  # - NOT div by 2 (rules out even numbers) or 3
-  # - ... how to rule out negative numbers ... 3,5,7,9...
+#My thought process:
   def prime? (integer)
+    # create array of 1 to integer
+    range_check = (1...integer+1).to_a
+    # create empty array to store results
+    divisable_number = []
 
-  end
+    #iterate to see if integer is divisibale by range_check numbers 
+      range_check.select do |num|  
+        if integer % num == 0
+          # if so, add into divisable_number array
+          divisable_number << "yes"
+        end
+      end
+    
+    #count if array has only 2 numbers divisibale. If yes, then Prime number
+    divisable_number.length == 2 ? true : false
+   end
